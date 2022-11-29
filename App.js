fd49303a67745520f1e4077c00bd3910ebd0d724
schmeb0704 <seanauditor0704@gmail.com>
2022-11-28 23:51:54 -0500

    display searched pokemon

--- App.js
+++ App.js
@@ -1,7 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Image } from 'react-native';
+import React from "react"
 
 export default function App() {
+  const [pokemon, setPokemon] = React.useState({
+    name: "",
+    type: "",
+    dexNum: "",
+    sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png"
+  })
+  const [searchMe, setSearchMe] = React.useState("pikachu")
+  const [toSearch, setSearch] = React.useState({
+    searchName: ""
+  })
+
+  React.useEffect(()=>{
+    fetch(`https://pokeapi.co/api/v2/pokemon/${searchMe}`)
+      .then(response => response.json())
+      .then(dataFromResponse => fetch(dataFromResponse.forms[0].url)
+        .then(response => response.json())
+        .then(formsResponse => setPokemon(prevPokemon => ({
+          ...prevPokemon,
+          name: formsResponse.name,
+          type: formsResponse.types[0].type.name,
+          dexNum: formsResponse.id,
+          sprite: formsResponse.sprites.front_default
+        })))
+      )
+  }, [searchMe])
+
+
   return (
     <View style={styles.mainPage}>
       <View style={styles.inputSearch}>
@@ -17,6 +45,14 @@ export default function App() {
         </View>
       </View>
       <StatusBar style="auto" />
+      <View>
+        <Image source={{uri: `${pokemon.sprite}` }} style={{width: 100, height: 100}} />
+      </View>
+      <View>
+        <Text>{pokemon.name}</Text>
+        <Text>{pokemon.type}</Text>
+        <Text>{pokemon.dexNum}</Text>
+      </View>
     </View>
   );
 }
